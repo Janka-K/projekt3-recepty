49ae61e45f92148eb2a87c4783688d374885649b
Janka K <janakorycanova@gmail.com>
2022-04-21 22:24:58 +0200

    Vygenerovani z dat obsah receptu

--- app.js
+++ app.js
@@ -17,3 +17,59 @@ recept-hodnoceni, recept-nazev, recept-popis.
 
 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
+
+
+function generateContent(){
+
+    let body = document.getElementById("recepty");
+
+    for (i = 0; i < recepty.length; i++){
+        let box = document.createElement('div');
+        box.className = "recept";
+        body.appendChild(box);
+
+        let content = document.createElement("div");
+        content.className = "recept-obrazek";
+        box.append(content);
+
+        let picture = document.createElement("img");
+        picture.src = recepty[i].img;
+        content.append(picture);
+
+        let textBox = document.createElement("div");
+        textBox.className = "recept-info";
+        box.append(textBox);
+
+        let header = document.createElement("h3");
+        header.innerHTML = recepty[i].nadpis;
+        textBox.append(header);
+
+
+    }
+}
+
+
+generateContent();
+
+
+
+/*
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+*/
